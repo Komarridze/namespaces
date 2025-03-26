e87577b9e199415288fa5411a8a7d1a774aa65f1
Komarridze <aychihuahua7@gmail.com>
2025-03-27 00:43:19 +0100

    new session key + migrations

--- knttk-dev/server.mjs
+++ knttk-dev/server.mjs
@@ -8,6 +8,8 @@ import {Server} from 'socket.io'
 import {createServer} from 'node:http'
 import express from 'express'
 
+import sessionKey from './session.js'
+
 import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser'
 import {Router} from 'express'
@@ -24,16 +26,16 @@ import { url } from 'node:inspector'
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 
-// const dbPromise = open({
-//     filename: "data.db",
-//     driver: sqlite3.Database
-// })
+const dbPromise = open({
+    filename: "namespace.db",
+    driver: sqlite3.Database
+})
 
 
 const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
-    secret: 'n4m35P4C3#key#xc08FG1',
+  secret: sessionKey,
   resave: false,
   saveUninitialized: false
 }))
@@ -51,19 +53,51 @@ const io = new Server(server)
 
 app.get('/', urlencodedParser, async(req, res) => {
 
-    res.render('main')
+    const db = await dbPromise;
+
+    let ns = '';
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    // Loop to generate characters for the specified length
+    for (let i = 0; i < 7; i++) {
+        let randomInd = Math.floor(Math.random() * characters.length);
+        ns += characters.charAt(randomInd);
+    }
+    
+
+    let namespaces = await db.all(`SELECT * FROM Namespaces WHERE namespace = '${ns}'` )
+
+    while (namespaces.length != 0) {
+        ns = '';
+        for (let i = 0; i < 7; i++) {
+            let randomInd = Math.floor(Math.random() * characters.length);
+            ns += characters.charAt(randomInd);
+        }
+        namespaces = await db.all(`SELECT * FROM Namespaces WHERE namespace = '${ns}'` )
+    }
+
+
+
+
+    res.render('main', {data:{namespace:ns}})
 
 
 })
 
-app.get('/auth', urlencodedParser, async(req, res) => {
-    res.render('auth-login')
+app.post('/auth', urlencodedParser, async(req, res) => {
+
+    let namespace = req.body.namespace;
+    console.log(`[ OK ] POST : auth > Request for :${req.body.namespace}:.`);
+
+    if (namespace == 'login') {res.render('auth-login')}
+    else {res.render('auth-create', {data:{namespace:namespace}})}
+
 })
 
 
 const setup = async () => {
-    // const db = await dbPromise;
-    // await db.migrate();
+    const db = await dbPromise;
+    await db.migrate();
     server.listen(5173, () => {
         console.log('Listening on port 5173...')
     })
